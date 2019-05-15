e6879db279c5f0ae6aee098ac368fd2799c48bc6
Olga Lavrichenko <lavrikolyalavrik@gmail.com>
2019-05-15 16:46:07 +0300

    + deconv

--- ResNet/main.py
+++ ResNet/main.py
@@ -2,7 +2,7 @@
 import numpy as np
 from keras.models import Sequential,Model
 from keras.layers import Conv2D, MaxPool2D, Deconv2D, Cropping2D
-from keras.layers import Input, Add, Dropout, Permute,  LeakyReLU, BatchNormalization, AveragePooling2D
+from keras.layers import Input, Add, Dropout, Permute,  LeakyReLU, BatchNormalization, AveragePooling2D, UpSampling2D
 from keras.layers import Dense, Flatten, Activation, Dropout, Embedding, Add,Concatenate,MaxPool2D
 from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator
@@ -11,13 +11,14 @@ import tensorflow as tf
 
 np.random.seed(42)
 
+a=Cropping2D()
 
 session_config = tf.ConfigProto( )
-train_dir = 'cloud_data/duckscapes/train'
+train_dir = 'data_set/DuckData/train'
 
-val_dir = 'cloud_data/duckscapes/val'
+val_dir = 'data_set/DuckData/val'
 
-test_dir = 'cloud_data/duckscapes/val'
+test_dir = 'data_set/DuckData/val'
 
 img_width, img_height = 150, 150
 
@@ -53,41 +54,57 @@ test_generator = datagen.flow_from_directory(
                                              batch_size=batch_size,
                                              class_mode='binary')
 
-def ResConv(kol_kanal , inp ):
-    a = Conv2D(kol_kanal,(1,1) ,padding="same", activation="relu", input_shape=(1,320,320) )(inp)
-    b = Conv2D(kol_kanal, (3, 3), padding="same", activation="relu", input_shape=(1, 320, 320))(a)
-    c = Conv2D(4*kol_kanal, (1, 1), padding="same", activation=None, input_shape=(1, 320, 320))(b)
-    d = Concatenate()([c,inp])
-    e=Activation('relu')(d)
+
+def ResConv(kol_kanal, inp):
+    a = Conv2D(kol_kanal, (1, 1), padding="same", activation="relu")(inp)
+    b = Conv2D(kol_kanal, (3, 3), padding="same", activation="relu")(a)
+    c = Conv2D(4 * kol_kanal, (1, 1), padding="same", activation=None)(b)
+    d = Concatenate()([c, inp])
+    e = Activation('relu')(d)
     return e
 
-input_shape=(150,150,3)
-inp0=Input(input_shape)
+
+def ResDeConv(inp):
+    a = UpSampling2D((1, 1))(inp)
+    b = UpSampling2D((3, 3))(a)
+    c = UpSampling2D((1, 1))(b)
+
+    d = Activation('relu')(c)
+    return d
+
+
+input_shape = (150, 150, 3)
+inp0 = Input(input_shape)
 
 model = Sequential()
 
+a = MaxPool2D((3, 3), 2)(inp0)
+skip1 = ResConv(32, a)
+a = MaxPool2D((3, 3), 2)(skip1)
+a = ResConv(32, a)
+a = MaxPool2D((2, 2), 2)(a)
+a = ResConv(32, a)
+a = MaxPool2D((2, 2))(a)
 
-a = MaxPool2D((3,3),2)(inp0)
-a = ResConv(32,a)
-a = ResConv(32,a)
-a = ResConv(32,a)
-a = ResConv(64,a)
-a = ResConv(64,a)
-a = ResConv(64,a)
+a = ResDeConv(a)
+a = Conv2D(256, (1, 1), padding="same", activation="relu")(a)
+a = ResDeConv(a)
 
-a=AveragePooling2D((1,1))(a)
-a=Flatten()(a)
-a=Dense(1000,activation='relu' )(a)
-
-a=Dropout(0.4)(a)
-a=Dense(1,activation='softmax')(a)
+a = Conv2D(131, (1, 1), padding="same", activation="relu")(a)
+a = Cropping2D((3, 3))(a)
+a = UpSampling2D((2, 2))(a)
+a = Conv2D(1, (1, 1), padding="same", activation="relu")(a)
 
 model = Model(inp0, a)
 
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 print(model.summary())
 
-
+model_json = model.to_json()
+json_file = open("mnist_model.json", "w")
+json_file.write(model_json)
+json_file.close()
+model.save_weights("model1.h5")
 model.fit_generator(
                     train_generator,
                     steps_per_epoch=nb_train_samples // batch_size,
